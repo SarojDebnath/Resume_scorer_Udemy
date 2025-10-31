62c577ebe9960c05e6764d8fb09f2aa4674fd393
sarojd <ripandebnath2405@gmail.com>
2025-10-31 11:24:59 +0100

    test

---
+++ exception/custom_exception.py
@@ -0,0 +1,25 @@
+import sys
+import traceback
+
+class ResumeanalyserException(Exception):
+    def __init__(self, error_message, error_details):
+        _, _, exc_tb = error_details.exc_info()
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        self.lineno = exc_tb.tb_lineno
+        self.error_message = str(error_message)
+        self.traceback_str = ''.join(traceback.format_exception(*error_details.exc_info()))
+
+    def __str__(self):
+        return f"""
+        Error in [{self.file_name}] at line [{self.lineno}]
+        Message: {self.error_message}
+        Traceback:
+        {self.traceback_str}
+        """
+
+if __name__ == "__main__":
+    try:
+        a = int("test")
+    except Exception as e:
+        app_exc = ResumeanalyserException(e, sys)
+        raise app_exc
